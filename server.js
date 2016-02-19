0abbe91beb53e525951a3853f2ace427bddfc6ba
Michael Jackson <mjijackson@gmail.com>
2016-02-19 15:59:20 -0800

    Remove server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-var port = process.env.PORT || process.env.npm_package_config_port
-var server = require('./index').createServer()
-
-server.listen(port, function () {
-  console.log('Server started on port ' + port + ', Ctrl+C to quit')
-})
