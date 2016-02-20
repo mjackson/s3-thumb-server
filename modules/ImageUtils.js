e7e16347cf02d98834d38547531af52f70f8ee83
Michael Jackson <mjijackson@gmail.com>
2016-02-19 16:24:00 -0800

    Add invariants

--- modules/ImageUtils.js
+++ modules/ImageUtils.js
@@ -1,3 +1,4 @@
+import invariant from 'invariant'
 import { S3 } from 'aws-sdk'
 import sharp from 'sharp'
 
@@ -5,6 +6,21 @@ const AWSAccessKeyID = process.env.AWS_ACCESS_KEY_ID
 const AWSSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 const AWSRegion = process.env.AWS_REGION || 'us-west-1'
 const S3Bucket = process.env.S3_BUCKET
+
+invariant(
+  AWSAccessKeyID,
+  'Missing $AWS_ACCESS_KEY_ID environment variable'
+)
+
+invariant(
+  AWSSecretAccessKey,
+  'Missing $AWS_SECRET_ACCESS_KEY environment variable'
+)
+
+invariant(
+  S3Bucket,
+  'Missing $S3_BUCKET environment variable'
+)
 
 const S3Client = new S3({
   accessKeyId: AWSAccessKeyID,
